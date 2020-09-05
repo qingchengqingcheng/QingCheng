7236e0ac1f15ee52626ae33212b8d07b145fc024
qingcheng <201326551@qq.com>
2020-09-05 15:10:23 +0800

    修改文件字符集并添加注释说明

--- user/src/main/java/com/qingcheng/user/Config/RestTemplateConfig.java
+++ user/src/main/java/com/qingcheng/user/Config/RestTemplateConfig.java
@@ -9,6 +9,13 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
 
+    /**
+     * @Discription TOOD 注入RestTemplate
+     * @author qingcheng
+     * @Date 2020/9/5 14:01
+     * RestTemplate 默认开启负载均衡 如果需要实现serviceId调用其他服务 需要添加@LoadBalanced注解
+     * @return: org.springframework.web.client.RestTemplate
+     */
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
