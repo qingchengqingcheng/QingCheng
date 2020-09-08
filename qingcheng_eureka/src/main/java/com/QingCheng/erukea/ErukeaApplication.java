819bb4af4a468d56f07c3e7dbc02ab6f6a948df2
qingcheng <201326551@qq.com>
2020-09-08 09:32:36 +0800

    修改项目结构

--- qingcheng_eureka/src/main/java/com/QingCheng/erukea/ErukeaApplication.java
+++ qingcheng_eureka/src/main/java/com/QingCheng/erukea/ErukeaApplication.java
@@ -1,7 +1,6 @@
-package com.QingCheng.erukea;
+package com.qingcheng.erukea;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
--- qingcheng_user/src/main/java/com/qingcheng/user/Controller/UserController.java
+++ qingcheng_user/src/main/java/com/qingcheng/user/Controller/UserController.java
@@ -1,8 +1,6 @@
 package com.qingcheng.user.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +15,7 @@ public class UserController {
 
     @GetMapping(value = "/getUserByAuth", produces = "application/json;charset=utf-8")
     public String getUserByAuth() {
-        String result = restTemplate.getForObject("http://qingcheng-auth/auth/getAuth", String.class);
+        String result = restTemplate.getForObject( "http://qingcheng-auth/auth/getAuth", String.class );
         return result;
     }
 }
